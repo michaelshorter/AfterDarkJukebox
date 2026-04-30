ba6ec391cd230812520ac567ff15f7f28a61b5dc
michaelshorter <mail@mrshorter.co.uk>
2026-04-30 15:16:42 +0100

    Change Python script to latest version
    Updated script to use the latest jukebox.py file instead of an older version.

--- run_jukebox.sh
+++ run_jukebox.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 export DISPLAY=:0
 export XAUTHORITY=/home/jukebox/.Xauthority
+
+# Hide the cursor at OS level so VLC can't bring it back
+unclutter -idle 0 -root &
+
+# Wait for USB drive to mount
+echo "Waiting for USB drive..."
+while [ ! -d "/media/jukebox/JUKEBOX/videos" ]; do
+    sleep 1
+done
+echo "USB drive found, starting jukebox..."
+
 source /home/jukebox/jukebox-env/bin/activate
-python /home/jukebox/jukebox_07042026.py
+python /home/jukebox/jukebox.py
